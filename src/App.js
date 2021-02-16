c2061bb3f6367e12335e121c3c80de642348963c
stevecode <suarezsteve21@gmail.com>
2021-02-15 20:14:37 -0500

    Some improvments

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const App = () => (
+  <h1 className='text-gray-400'>Welcome to AgroEncuentro!</h1>
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,41 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 module.exports = {
-    output: {
-        filename: 'app.bundle.js',
+  output: {
+    filename: 'app.bundle.js'
 
-    },
-    plugins: [
-new HtmlWebpackPlugin({
-    template: 'src/index.html'
-})
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            '@babel/preset-react'
-                        ]
-                    }
-                }
-            }
-        ]
-    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: ['style-loader', 'postcss-loader']
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    hot: true
+  }
 }
