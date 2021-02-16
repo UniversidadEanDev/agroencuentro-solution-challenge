d477d4454211da7fe4aa7ba03a35e1c831d47b21
stevecode <suarezsteve21@gmail.com>
2021-02-15 19:30:13 -0500

    Webpack swetups2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render('Hello DSC', document.getElementById('app'))
+ReactDOM.render(<h1>Welcome to AgroEncuentro!</h1>, document.getElementById('app'))
